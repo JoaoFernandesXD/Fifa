0a4948710eaef8da02a2d383a12d59c2824fbac2
Joao Fernandes <joaofernandesxd1@icloud.com>
2025-07-22 18:25:41 -0300

    Ajustando projeto e criando o components header

---
+++ src/components/Composition.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted, computed, reactive } from 'vue'
+
+const name = ref('John')
+
+const personagem = reactive({
+    nome: 'Jason Duval',
+    idade: 20,
+    sexo: 'Masculino'
+})
+
+</script>
+
+ 
+<template>
+    <h1>Composition</h1>
+    <h3>{{ personagem.nome }}</h3>
+    <input type="text" v-model="personagem.nome">
+    <input type="text" v-model="personagem.idade">
+    <input type="text" v-model="personagem.sexo">
+    <br>
+    
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/ui/TheHeader.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <div class="container section--container">
+            <img src="../../assets/logo-fifa.png" alt="Logo oficial fifa 2022">
+            <nav>
+                <a href="#">Início</a>
+                <a href="#">Grupos</a>
+                <a href="#">Partidas</a>
+                <a href="#">Assistir</a>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+
+header {
+    width: 100%;
+    height: 96px;
+    background-color: var(--dark-30);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+header .container {
+    width: 100%;
+    max-width: 1216px;
+    height: 24px;
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+
+}
+nav {
+    width: 100%;
+    max-width: 414px;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+nav a {
+    font-weight: 400;
+    font-size: 1.1rem;
+    color: var(--dark-20);
+}
+nav a:hover {
+    color: var(--brand-color);
+}
+
+</style>
